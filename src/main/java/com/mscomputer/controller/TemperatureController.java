c7fb8202bb38704e64297f3b11bf52934e557ddb
Matus Moura <matus.moura333x2@gmail.com>
2024-06-02 12:06:05 -0300

    implementação do metodo GetAll

--- src/main/java/com/mscomputer/controller/TemperatureController.java
+++ src/main/java/com/mscomputer/controller/TemperatureController.java
@@ -1,8 +1,12 @@
 package com.mscomputer.controller;
 
+import java.io.File;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +26,11 @@ public class TemperatureController {
 	public ResponseEntity<String> receiveInformationGround(@RequestBody TemperatureData temperatureData){
 		repository.save(temperatureData);
 		return ResponseEntity.ok("Info received");
+	}
+	
+	@GetMapping("/tempget")
+	public ResponseEntity<List<TemperatureData>> getAllProducts() {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
 	}
 	
 }
--- src/main/java/com/mscomputer/model/TemperatureData.java
+++ src/main/java/com/mscomputer/model/TemperatureData.java
@@ -12,76 +12,49 @@ import jakarta.persistence.Id;
 public class TemperatureData {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
 	private float temperature;
 	private float humidity;
 	private LocalDateTime time;
-	
-	
 	
 	public TemperatureData() {
 		this.time = LocalDateTime.now();
 	}
 
-
-
-	public TemperatureData(Long id, float temperature, float humidity) {
+	public TemperatureData(UUID id, float temperature, float humidity) {
 		this.id = id;
 		this.temperature = temperature;
 		this.humidity = humidity;
 		this.time = LocalDateTime.now();
 	}
 
-
-
-	public Long getId() {
+	public UUID getId() {
 		return id;
 	}
-
-
-
-	public void setId(Long id) {
+	public void setId(UUID id) {
 		this.id = id;
 	}
-
-
 
 	public float getTemperature() {
 		return temperature;
 	}
-
-
-
 	public void setTemperature(float temperature) {
 		this.temperature = temperature;
 	}
-
-
-
+	
 	public float getHumidity() {
 		return humidity;
 	}
-
-
-
 	public void setHumidity(float humidity) {
 		this.humidity = humidity;
 	}
 
-
-
 	public LocalDateTime getTime() {
 		return time;
 	}
-
-
-
 	public void setTime(LocalDateTime time) {
 		this.time = time;
 	}
-	
-	
-	
 	
 }
